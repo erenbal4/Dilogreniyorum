ad88faca43cae8f993d28427466c5fabecb6ade8
erenbal4 <44erenbal44@gmail.com>
2026-03-05 08:56:28 +0400

    Create Home component for language learning page

---
+++ Pages/İndex.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold">Dil Öğreniyorum</h1>
+      <p>Hoşgeldin! Başlamak için giriş yap veya kayıt ol.</p>
+    </div>
+  );
+}
